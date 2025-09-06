c4e857b744ffe16c2b843719f2a23b0fe4769d87
arman <armanjmi2022@gmail.com>
2025-09-06 16:30:38 +0530

    initial commit

--- app/src/main/java/com/example/transcriber/di/ApiKey.kt
+++ app/src/main/java/com/example/transcriber/di/ApiKey.kt
@@ -2,5 +2,5 @@ package com.example.transcriber.di
 
 object ApiKey {
     //your whisper key here
-    val apiKey="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"
+    val apiKey="your api key here"
 }
